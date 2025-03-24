548654c02a7730fceb1fe2ca12d9d5d7fc14dec3
Stranger484 <gladstranger@gmail.com>
2025-03-24 11:28:19 +0700

    Create project

---
+++ VKParser.dpr
@@ -0,0 +1,15 @@
+program VKParser;
+
+uses
+  Vcl.Forms,
+  UnitMain in 'UnitMain.pas' {MainForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
+
